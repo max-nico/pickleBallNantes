e025899eb99cd4703f445786ae11edc88c74560a
Nicolas <thugnic@gmail.com>
2019-07-19 10:28:10 +0200

    change layout default page

--- page-acceuil.php
+++ page-acceuil.php
@@ -5,11 +5,10 @@
 <?php get_header();?>
 <div class="default_page">
     <?php  if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-        <div class="container">
+        <div class="container-fluid">
             <?php the_content();  ?>
             <div class="clearfix"></div>
         </div>
     <?php endwhile; endif; ?>
-    <?php get_template_part( 'framework/content/acceuil/action-blocks'); ?>
 </div>
 <?php  get_footer(); ?>
