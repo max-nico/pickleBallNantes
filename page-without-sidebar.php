067ca5c33b62ad1d0ec219e63993965242eeaa70
Nicolas <thugnic@gmail.com>
2019-07-19 10:43:04 +0200

    change layout default page

---
+++ page-without-sidebar.php
@@ -0,0 +1,9 @@
+<?php get_header(); ?>
+    <div class="container-fluid content-wrap">
+        <div class="row">
+                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 no-sidebar">
+                <?php get_template_part( 'framework/content/page');?>
+                </div>    
+        </div>                
+    </div>
+<?php get_footer(); ?>
