86ca62f5454262595cb9a3e0a52f4c5a462cba1a
Nicolas <thugnic@gmail.com>
2019-07-19 13:01:31 +0200

    style for page qui sommes nous v 0.0.3

--- header.php
+++ header.php
@@ -4,6 +4,8 @@
   <meta charset="<?php bloginfo( 'charset' ); ?>">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="profile" href="http://gmpg.org/xfn/11">
+  <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBBFZTkrtNLmpc0AeGTUnLtqp5DEvPT1nc&callback=initMap"
+  type="text/javascript"></script>
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
   <?php $allowed_html_array = wp_kses_allowed_html( 'post' )?>
   <?php if ( ! ( function_exists( 'has_site_icon' ) && has_site_icon() ) ) {?><link rel="shortcut icon" href="<?php echo esc_url(get_theme_mod('insomnia_logo_favicon', get_template_directory_uri() . '/assets/images/favicon.png')); ?>"> <?php };?>
