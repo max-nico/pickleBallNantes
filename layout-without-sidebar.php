9c3649f3fe50ac5d31da742b4683024933320f85
Nicolas <thugnic@gmail.com>
2019-07-19 10:57:15 +0200

    change layout default page

--- layout-without-sidebar.php
+++ layout-without-sidebar.php
@@ -6,7 +6,7 @@ Template Post Type: page
 ?>
 <?php get_header(); ?>
     <div class="container-fluid content-wrap">
-        <div class="row">
+        <div class="row" id="main">
                 <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 no-sidebar">
                 <?php get_template_part( 'framework/content/page');?>
                 </div>    
