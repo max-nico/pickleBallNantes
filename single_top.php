19dadf1a9204d97ddde96f528363f155a2f2297f
nicolas.squeren@viacesi.fr <nicolas@stock.ovh>
2019-08-06 08:18:23 +0200

    change on ftp to work

--- single_top.php
+++ single_top.php
@@ -5,7 +5,7 @@ Templtate name: landing-top
 ?>
 
 <?php get_header(); ?>
-<div class="container content-wrap">
+<div class="container-fluid content-wrap">
     <div class="row">
         <?php $layout_value = get_theme_mod( 'insomnia_sidebars', 'sidebar-right' ); ?>
         <div class="col-lg-12 col-md-12 col col-sm-12 col-xs-12 sidebar-left">
