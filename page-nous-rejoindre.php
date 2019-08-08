1b3587cdfb4fe18fa93e5a16684cfb8e49206c97
nicolas.squeren@viacesi.fr <nicolas@stock.ovh>
2019-08-09 00:46:38 +0200

    affichage page nous rejoindre - a finialiser

--- page-nous-rejoindre.php
+++ page-nous-rejoindre.php
@@ -1,4 +1,5 @@
 <?php get_header(); ?>
+<script src="../wp-content/themes/pickleball/assets/js/navBarStateColor.js"></script>
     <?php  if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
     <div class="container-full learning">
         <div class="row">
@@ -6,4 +7,9 @@
         </div>
         <?php endwhile; endif; ?>
     </div>              
+    <script style="text/javascript">
+        document.querySelectorAll('.insomnia_vc_button').forEach(element => {
+            element.className += " btn-e-learning"
+        });
+    </script>
 <?php get_footer(); ?>
