78c57fd1153c7697499d7e3cb89030b2136e7cbf
nicolas.squeren@viacesi.fr <nicolas@stock.ovh>
2019-08-08 22:10:33 +0200

    se-former modif + nous rejoindre

--- page-nous-rejoindre.php
+++ page-nous-rejoindre.php
@@ -2,10 +2,7 @@
     <?php  if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
     <div class="container-full learning">
         <div class="row">
-        <?php if(!empty($value['main_video'])) : ?>
-            <?= $value['main_video'];?>
-            <a href="<?php get_permalink($post, $leavename) ?>" class="btn-danger btn-e-learning">Nous rejoindre</a>
-        <?php endif; ?>
+            <?php the_content(); ?>
         </div>
         <?php endwhile; endif; ?>
     </div>              
--- page-se-former.php
+++ page-se-former.php
@@ -1,16 +1,10 @@
 <?php get_header(); ?>
-<script src="../wp-content/themes/pickleball/assets/js/navBarStateColor.js"></script>
     <?php  if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
     <div class="container-full learning">
         <div class="row">
             <?php the_content(); ?>
-            <!-- <a href="<?php get_permalink($post, $leavename) ?>" class="btn-danger btn-e-learning">e-learning</a> -->
+            <a href="<?php get_permalink($post, $leavename) ?>" class="btn-danger btn-e-learning">e-learning</a>
         </div>
         <?php endwhile; endif; ?>
     </div>              
-<script type="text/javascript">
-document.querySelectorAll("a.insomnia_vc_button").forEach(element => {
-    element.className += " btn-danger btn-e-learning";
-});
-</script>
 <?php get_footer(); ?>
